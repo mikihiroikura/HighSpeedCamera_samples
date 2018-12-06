7ca35eb1ea2539ebdd9da7eee930e48c148898de
mikihiroikura <38698038+mikihiroikura@users.noreply.github.com>
2018-12-06 17:57:43 +0900

    光切断法の更新，MATLAB呼び出し，Log整理

--- sample/ShapeMeasurement/main.cpp
+++ sample/ShapeMeasurement/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
 #include <thread>
 #include <string>
 #include <vector>
@@ -7,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <direct.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <Windows.h>
 
 #ifdef _DEBUG
 #define CAMERA_EXT "d.lib"
@@ -26,37 +30,51 @@
 
 #pragma warning(disable:4996)
 
+//DEFINE群
+//#define SAVE_IMG_         //画像と動画をLogに残す
+#define MATLAB_GRAPHICS_  //MATLABを起動し，Logをプロットする
+
 using namespace std;
 
 struct Capture
 {
-	bool flag;
-	basler cam;
-	cv::Mat in_img;
-	cv::Mat out_img;
+	bool flag;//メインループ実行フラグ
+	unsigned int pic_cnt;//処理カウンター
+	basler cam;//Baslerクラス
+	cv::Mat in_img;//光切断法入力画像
+	cv::Mat out_img;//光切断画像処理出力画像
 	float rob_ini_height = 30.0f;//mm
 	float height_interval = 10.0f;//mm
 	float height = rob_ini_height;//mm
-	vector<double> CoGs;
-	vector<vector<double>> IdealPixs;
-	vector<double> lambdas;
-	vector<double> Heights_cam;
-	vector<double> Heights_world;
-	vector<cv::Mat> Worlds;
+	vector<double> CoGs;//行ごとの輝度重心座標
+	vector<vector<double>> IdealPixs;//歪み補正ピクセル座標
+	vector<double> lambdas;//直線の式の倍率
+	vector<double> Heights_cam;//高度@カメラ座標系
+	vector<double> Heights_world;//高度@World座標系
+	vector<cv::Mat> Worlds;//World座標系
 	vector<int> row_num;
 	bool max_h_flg = false;
+	//魚眼レンズ内部パラメーター
 	vector<double> map_coefficient;
 	vector<double> stretch_mat;
 	vector<double> distortion;
 	vector<double> laser_plane;
 	vector<cv::Mat> Rots;
 	vector<double> Rot_height;
+	//Log格納用配列
 	vector<double> Log_times;
 	vector<vector<int>> Row_num_Logs;
 	vector<vector<double>> Height_Logs;
+	vector<vector<cv::Mat>> Worlds_Logs;
+	vector<cv::Mat> Pictures;
+	vector<bool> Processed;
 };
 //グローバル変数
 double result, logtime ,det;
+//光切断法範囲指定
+int sta_row = 80;
+int end_row = 400;
+int cnt_row = (end_row - sta_row) / 2;
 
 
 //プロトタイプ宣言
@@ -77,7 +95,8 @@ int main() {
 	Capture cap;
 	cap.in_img = (cv::Mat(height, width, CV_8UC1, cv::Scalar::all(255)));
 	cap.flag = true;
-
+	cap.pic_cnt = 0;
+	cap.Processed.push_back(false);//先にFalseを追加しておく
 	cap.cam.connect(0);
 	cap.cam.setParam(paramTypeCamera::paramInt::WIDTH, width);
 	cap.cam.setParam(paramTypeCamera::paramInt::HEIGHT, height);
@@ -134,28 +153,37 @@ int main() {
 	//結果保存用ファイルを日時付きで作成し開く
 	FILE* fr;
 	time_t timer;
+	struct tm now; 
 	struct tm *local;
 	timer = time(NULL);
-	local = localtime(&timer);
+	localtime_s(&now, &timer);
+	char dir0[256];
+	strftime(dir0, 256, "C:/Users/Mikihiro Ikura/Documents/GitHub/HighSpeedCamera/sample/ShapeMeasurement/results/%y%m%d", &now);
+	_mkdir(dir0);
 	char dir[256];
-	sprintf(dir, "C:/Users/Mikihiro Ikura/Documents/GitHub/HighSpeedCamera/sample/ShapeMeasurement/results/%4d%2d%2d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday);
+	strftime(dir, 256, "C:/Users/Mikihiro Ikura/Documents/GitHub/HighSpeedCamera/sample/ShapeMeasurement/results/%y%m%d/%H%M%S", &now);
 	_mkdir(dir);
+	char dir2[256];
+	strftime(dir2, 256, "C:/Users/Mikihiro Ikura/Documents/GitHub/HighSpeedCamera/sample/ShapeMeasurement/results/%y%m%d/%H%M%S/Pictures", &now);
+	_mkdir(dir2);
 	char filename[256];
-	sprintf(filename, "results/%4d%2d%2d/%2d%2d_LS_result.csv", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday, local->tm_hour, local->tm_min);
+	strftime(filename, 256, "results/%y%m%d/%H%M%S/LS_result.csv", &now);
 	fr = fopen(filename, "w");
 	//
 	bool flag = true;
 	thread thr(TakePicture, &cap, &flag);
 	thread thr2(OutPutLogs, &cap, &flag);
-
+	Sleep(1);//threadのみ1ms実行し，画像を格納させる
 	if (!QueryPerformanceCounter(&start)) { return 0; }
 	//メインループ
 	while (flag)
 	{
-		CalcHeights(&cap);//高度計算
-		if (!QueryPerformanceCounter(&end)) { return 0; }
-		logtime = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
-		cap.Log_times.push_back(logtime);
+		if (cap.Processed[cap.pic_cnt]){//写真があるときに
+			CalcHeights(&cap);//高度計算
+			if (!QueryPerformanceCounter(&end)) { return 0; }
+			logtime = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
+			cap.Log_times.push_back(logtime);
+		}
 	}
 	if (thr.joinable())thr.join();
 	if (thr2.joinable())thr2.join();
@@ -164,31 +192,80 @@ int main() {
 	cap.cam.disconnect();
 
 	//cap内のLogを保存
-	printf("saving Logs...\n");
-	for (size_t i = 0; i < cap.Height_Logs.size(); i++){
-		fprintf(fr, "%lf,", cap.Log_times[i]);
-		for (size_t j = 0; j < cap.Height_Logs[i].size(); j++){
+	printf("saving Logs...   ");
+	for (int i = 0; i < cap.Log_times.size(); i++){
+		fprintf(fr, "%lf,\n", cap.Log_times[i]);
+		for (int j = 0; j < cap.Row_num_Logs[i].size(); j++){
 			fprintf(fr, "%d,", cap.Row_num_Logs[i][j]);
 		}
 		fprintf(fr, "\n");
-		for (size_t j = 0; j < cap.Height_Logs[i].size(); j++){
-			fprintf(fr, "%lf,", cap.Height_Logs[i][j]);
+		for (int j = 0; j < cap.Worlds_Logs[i].size(); j++){
+			fprintf(fr, "%lf,", cap.Worlds_Logs[i][j].at<double>(0,0));
+		}
+		fprintf(fr, "\n");
+		for (int j = 0; j < cap.Worlds_Logs[i].size(); j++) {
+			fprintf(fr, "%lf,", cap.Worlds_Logs[i][j].at<double>(0, 1));
+		}
+		fprintf(fr, "\n");
+		for (int j = 0; j < cap.Worlds_Logs[i].size(); j++) {
+			fprintf(fr, "%lf,", cap.Worlds_Logs[i][j].at<double>(0, 2));
 		}
 		fprintf(fr, "\n");
 	}
-	printf("finish!\n");
+	printf("Logs finish!\n");
+	fclose(fr);
 
+	//画像を動画保存
+	#ifdef SAVE_IMG_
+	printf("saving Imgs...   ");
+	char videoname[256];
+	char picname[256];
+	char picsubname[128];
+	strftime(videoname, 256, "results/%y%m%d/%H%M%S/video.avi", &now);
+	strftime(picsubname, 128, "results/%y%m%d/%H%M%S/Pictures/frame", &now);
+	cv::VideoWriter writer(videoname, cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), 750.0, cv::Size(640, 480), false);//モノクロなのでFalse指定
+	if (!writer.isOpened()) { return -1; }
+	for (int i = 0; i < cap.Pictures.size(); i++) {
+		writer << cap.Pictures[i].clone();
+		sprintf(picname, "%s%d.jpg", picsubname, i);
+		cv::imwrite(picname, cap.Pictures[i]);
+	}
+	printf("Imgs finish!\n");
+	writer.release();
+	#endif // SAVE_IMG_
+
+	//MATLABでLogの出力をする
+	#ifdef MATLAB_GRAPHICS_
+	printf("MATLAB Open...   ");
+	int ret = 0;
+	char matdir[256] = "C:\\Users\\Mikihiro Ikura\\Documents\\GitHub\\ShowLSLogs\\LS_result.csv";
+	int cpy = CopyFileA(filename, matdir, FALSE);
+	if (cpy) { printf("Copy success!   "); }
+	else { printf("Copy failed...   "); }
+	char matlabcmd[256] = "matlab -sd \"C:\\Users\\Mikihiro Ikura\\Desktop\" -r plot_sin -nosplash";
+	ret = system(matlabcmd);
+	if (ret == 0) { printf("MATLAB activate! \n"); }
+	else{printf("error! \n");}
+	#endif // MATLAB_GRAPHICS_
+
+	printf("ALL FINALIZE ! \n");
+	system("pause");
 	return 0;
 }
 
 //thread内の関数
+//ただ，capに写真を格納する
 void TakePicture(Capture *cap, bool *flg) {
 	while (*flg) {
-		cap->cam.captureFrame(cap->in_img.data);
+		cap->Pictures.push_back((cv::Mat(480, 640, CV_8UC1, cv::Scalar::all(255))));
+		cap->cam.captureFrame(cap->Pictures[cap->Pictures.size()-1].data);
+		cap->Processed[cap->Pictures.size() - 1] = true;//今追加された写真の番号部分はTrueにする
+		cap->Processed.push_back(false);//最新番号の次の番号部分はFalseとしておく
 	}
 }
 
 //thread2内の関数
+//標準出力で現在の写真，時刻と高度
 void OutPutLogs(Capture *cap ,bool *flg) {
 	while (1)
 	{
@@ -198,25 +275,28 @@ void OutPutLogs(Capture *cap ,bool *flg) {
 			*flg = false;
 			break;
 		}
-		if (cap->Height_Logs[cap->Height_Logs.size() - 1].size() == 0) { printf("Time: %lf s  Height: NONE\n", &logtime); }
-		else {
-			result = cap->Height_Logs[cap->Height_Logs.size() - 1][0];
-			printf("Time: %lf s  Height: %lf mm\n", logtime, result);
+		if (cap->Height_Logs.size()>0){
+			if (cap->Height_Logs[cap->Height_Logs.size() - 1].size() == 0) { printf("Time: %lf s  Height: NONE\n", &logtime); }
+			else {
+				result = cap->Worlds_Logs[cap->Worlds_Logs.size() - 1][0].at<double>(0, 2);
+				printf("Time: %lf s  Height: %lf mm\n", logtime, result);
+			}
 		}
+		
 	}
 }
 
-//main loop内でのh高度計算
+//main loop内での高度計算
 void CalcHeights(Capture *cap) {
+	//前回使用した配列のClear
 	cap->CoGs.clear();
 	cap->Heights_cam.clear();
 	cap->Heights_world.clear();
 	cap->IdealPixs.clear();
 	cap->Worlds.clear();
 	cap->row_num.clear();
-	//輝度重心計算する行の範囲指定
-	int sta_row = 200;
-	int end_row = 280;
+	//画像配列群から次の処理カウンターの番号の画像をin_imgに格納する
+	cap->in_img = cap->Pictures[cap->pic_cnt];	
 	//輝度重心の計算
 	cv::threshold(cap->in_img, cap->out_img, 150.0, 255.0, cv::THRESH_BINARY);
 	cv::bitwise_and(cap->in_img, cap->out_img, cap->out_img);
@@ -229,17 +309,17 @@ void CalcHeights(Capture *cap) {
 			moment += j * cap->out_img.data[i*cap->out_img.cols + j];
 		}
 		if (mass <= 0) { cog = 0; }
-		else { 
+		else {
 			cog = moment / mass;
 			cap->row_num.push_back(i);
 		}
 		cap->CoGs.push_back(cog);
-		
+
 	}
 	//輝度重心⇒理想ピクセル座標に変換
 	vector<double> idpixs;
-	double u, v,w;//次のfor内でも使う
-	double ud,vd;
+	double u, v, w;//次のfor内でも使う
+	double ud, vd;
 	for (int i = 0; i < cap->CoGs.size(); i++)
 	{
 		if (cap->CoGs[i] == 0) { continue; }
@@ -253,13 +333,13 @@ void CalcHeights(Capture *cap) {
 		idpixs.clear();
 	}
 	//理想ピクセル座標⇒直線の式とレーザー平面の求解
-	double phi,lambda,h;
+	double phi, lambda, h;
 	for (size_t i = 0; i < cap->IdealPixs.size(); i++)
 	{
 		u = cap->IdealPixs[i][0];
 		v = cap->IdealPixs[i][1];
 		phi = pow((pow(u, 2) + pow(v, 2)), 0.5);
-		w = cap->map_coefficient[0] + cap->map_coefficient[1] * pow(phi, 2) 
+		w = cap->map_coefficient[0] + cap->map_coefficient[1] * pow(phi, 2)
 			+ cap->map_coefficient[2] * pow(phi, 3) + cap->map_coefficient[3] * pow(phi, 4);
 		lambda = -cap->laser_plane[2] / (cap->laser_plane[0] * u + cap->laser_plane[1] * v - w);
 		cap->lambdas.push_back(lambda);
@@ -271,24 +351,27 @@ void CalcHeights(Capture *cap) {
 	//int min_idx=0;
 	/*for (int j = 0; j < cap->Rot_height.size(); j++)
 	{
-		if (cap->Heights.size() == 0) { continue; }
-		if (min_h>abs(cap->Heights[0]-cap->Rot_height[j]))
-		{
-			min_h = abs(cap->Heights[0] - cap->Rot_height[j]);
-			min_idx = j;
-		}
+	if (cap->Heights.size() == 0) { continue; }
+	if (min_h>abs(cap->Heights[0]-cap->Rot_height[j]))
+	{
+	min_h = abs(cap->Heights[0] - cap->Rot_height[j]);
+	min_idx = j;
+	}
 	}*/
 	//カメラ座標系中心のWorld座標に変換
-	cv::Mat campnt,wldpnt;
+	cv::Mat campnt, wldpnt;
 	double hw;
 	for (size_t i = 0; i < cap->Heights_cam.size(); i++)
 	{
-		campnt = (cv::Mat_<double>(1, 3) << cap->lambdas[i]*cap->IdealPixs[i][0], cap->lambdas[i] * cap->IdealPixs[i][1], cap->Heights_cam[i]);
+		campnt = (cv::Mat_<double>(1, 3) << cap->lambdas[i] * cap->IdealPixs[i][0], cap->lambdas[i] * cap->IdealPixs[i][1], cap->Heights_cam[i]);
 		wldpnt = campnt * cap->Rots[0];
-		hw = wldpnt.clone().at<double>(0,2);
+		hw = wldpnt.clone().at<double>(0, 2);
 		cap->Worlds.push_back(wldpnt.clone());
 		cap->Heights_world.push_back(hw);
 	}
 	cap->Height_Logs.push_back(cap->Heights_world);
+	cap->Worlds_Logs.push_back(cap->Worlds);
 	cap->Row_num_Logs.push_back(cap->row_num);
+	//処理カウンターと処理判別の更新
+	cap->pic_cnt++;	
 }
